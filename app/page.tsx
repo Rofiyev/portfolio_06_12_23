15e5b1d6ce554ad91936a7457bfdff43a38e5b7d
Rof1yev <rofiyevdilshod@gmail.com>
2023-12-06 23:18:57 +0500

    Background animation and Hero section completed

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,11 @@
-export default function Home() {
-  return <main></main>;
+import { Hero } from "@/components";
+
+export default function Home(): JSX.Element {
+  return (
+    <main className="h-full w-full">
+      <div className="flex flex-col h-full gap-20">
+        <Hero />
+      </div>
+    </main>
+  );
 }
